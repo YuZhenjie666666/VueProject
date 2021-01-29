02391be24cc3212c7087a0e9251e19d6a0a9836b
YuZhenjie <15716356549@163.com>
2021-01-29 15:03:11 +0800

    修改tabbar

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,32 @@
   <div class="app-container">
     <!-- 这是顶部信息 -->
     <mt-header fixed title="教师评价系统"></mt-header>
+
+
+
+
+
     <!-- 这是底部消息 -->
     <nav class="mui-bar mui-bar-tab">
-      <a class="mui-tab-item mui-active" href="#tabbar">
+      <router-link class="mui-tab-item mui-active" to="/home">
         <span class="mui-icon mui-icon-home"></span>
         <span class="mui-tab-label">首页</span>
-      </a>
-      <a class="mui-tab-item" href="#tabbar-with-chat">
+      </router-link>
+      <router-link class="mui-tab-item" to="/member">
         <span class="mui-icon mui-icon-contact"> </span>
         <span class="mui-tab-label">会员</span>
-      </a>
-      <a class="mui-tab-item" href="#tabbar-with-contact">
+      </router-link>
+      <router-link class="mui-tab-item" to="/shopcar">
         <span class="mui-icon mui-icon-extra mui-icon-extra-cart">
           <span class="mui-badge">0</span>
         </span>
 
         <span class="mui-tab-label">购物车</span>
-      </a>
-      <a class="mui-tab-item" href="#tabbar-with-map">
+      </router-link>
+      <router-link class="mui-tab-item" to="/search">
         <span class="mui-icon mui-icon-search"></span>
         <span class="mui-tab-label">搜索</span>
-      </a>
+      </router-link>
     </nav>
     <h1>这是 App 组件</h1>
   </div>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 // 入口文件
 import Vue from 'vue'
+// 导入路由模块
+// 1.1导入路由的包
+import VueRouter from 'vue-router'
+// 1.2启用路由
+Vue.use(VueRouter)
+// 1.3导入自己的路由模块
+import router from './router.js'
+
 
 // 导入mui组件
 import  './lib/mui/css/mui.min.css'
@@ -11,12 +19,11 @@ import { Header } from 'mint-ui'
 Vue.component(Header.name,Header)
 
 
-
-
 //导入App根组件
 import app from './App.vue'
 
 var vm = new Vue({
     el:'#app',
-    render : c => c(app)
+    render : c => c(app),
+    router//1.4挂在路由对象到项目中去
 })
